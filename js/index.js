4eefba0128243532635e94d61fbd5d9e85ae491a
zhou <275342555@qq.com>
2018-02-06 15:29:15 +0800

    初版，基本功能实现完成，bug还未调整，代码、结构还未优化

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
 var swiper = new Swiper('.swiper-container', {
+				initialSlide:1,
 				pagination: {
 					el: '.swiper-pagination',
 					dynamicBullets: true,
+					
 				},
 			});
+			
+var music = {};
+$(function(){
+	$.ajax({
+		type:"get",
+		url:"js/music.json",
+		async:true,
+		success:function (data){
+			music = data;
+			var str = '';
+			var length = music.list.length;
+			console.log(music);
+			for (var index in music.list) {
+				str += '<li data-id="">' + music.list[index].name + '<i></i></li>'
+			}
+			$(".swiper-slide_list ul").html(str);
+		}
+	});
+	
+	$(".swiper-slide_list ul").on("click","li",function(){
+		var thisEq = $(this).data("id");
+		//console.log(thisEq);
+		//alert(thisEq);
+		$(this).addClass("on").siblings().removeClass("on");
+	})
+	
+	
+	
+})
